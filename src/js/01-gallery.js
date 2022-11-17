da17257c95ca326b065a9ff954a39828961a922e
TetianaMamzer <tetianamamzer@gmail.com>
2022-11-17 18:01:42 +0200

    Виконала дз

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import Simplelightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const galleryCreate = document.querySelector('div.gallery');
+const galleryCard = createCard(galleryItems);
+
+console.log(galleryCard);
+galleryCreate.insertAdjacentHTML('beforeend', galleryCard);
+galleryCreate.addEventListener('click', onCardGallery);
+
+function createCard(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+      </a>
+   </li>
+    `;
+    })
+    .join('');
+}
+
+function onCardGallery(event) {
+  event.preventDefault();
+  window.addEventListener('keydown', closeModalKeydown);
+  const findGalleryCard = event.target.classList.contains('gallery__image');
+
+  if (!findGalleryCard) {
+    return;
+  }
+
+  event.target.removeAttribute('data-source');
+  const urlImgCard = event.target.getAttribute('data-source');
+  const desImgCard = event.target.getAttribute('alt');
+}
+
+function closeModalKeydown(e) {
+  if (e.code === 'Escape') {
+    lightbox.close(() => {
+      window.removeEventListener('keydown', closeModalKeydown);
+    });
+  }
+}
+
+const lightbox = new Simplelightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  closeText: '🐠',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const player = new Player(iframe);
 
+load();
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  console.log(data);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data))
+};
+
+function getTime() {
+const saveTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+return saveTime || [];
+};
+
+function load() {
+create = getTime();
+console.log(create.seconds)
+player.setCurrentTime(create.seconds || 0);
+};
+
+
+
+    
+    
